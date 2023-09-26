f5db4f1635536a39965c9989a07b8e78a6e5319c
Neisanael <91104239+Neisanael@users.noreply.github.com>
2023-09-26 10:56:36 +0700

    buat database

---
+++ app/Database/Migrations/2023-09-26-030121_Kamar.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Kamar extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField('id');
+        $this->forge->addField([
+            'nama'       => ['type' => 'varchar', 'constraint' => 255],
+            'fasilitas'      => ['type' => 'text'],
+            'harga'    => ['type' => 'double'],
+            'deskripsi' => ['type' => 'text'],
+            'status' => ['type' => 'int', 'constraint' => 1],
+            'gambar' => ['type' => 'varchar', 'constraint' => 255],
+            'created_at' => ['type' => 'datetime', 'null' => true],
+            'updated_at' => ['type' => 'datetime', 'null' => true],
+        ]);
+        
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('kamar');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('kamar');
+    }
+}
