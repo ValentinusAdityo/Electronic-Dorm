83d13d37e5509057493db3ab5c188589415fbea4
ValentinusAdityo <136043378+ValentinusAdityo@users.noreply.github.com>
2023-10-24 09:12:48 +0700

    a

--- app/Views/layout/navbarGuest.php
+++ app/Views/layout/navbarGuest.php
@@ -9,9 +9,8 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="/">
-                    <img src="images/logo_dreambiru.png" height="50" alt="logo">
+                    <img src="images/logo_dreambiru.png" height="32" alt="logo">
                 </a>
-
             </div>
             <nav id="main_menu" class="mobile_menu navbar-collapse">
                 <ul class="nav navbar-nav">
