85ea7b39944a0467e839df363817769c0e1812e4
androwilfridus <145644929+androwilfridus@users.noreply.github.com>
2023-12-02 23:57:48 +0700

    profil

--- app/Views/home/profil.php
+++ app/Views/home/profil.php
@@ -17,41 +17,90 @@
 <head>
     <title>Biodata Diri</title>
     <link rel="stylesheet" type="text/css" href="profil.css" />
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+        
+        .info {
+            text-align: left;
+            color: #333;
+            padding: 10px;
+        }
+
+        table {
+            width: 80%;
+            margin-top: 10px;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            padding: 15px;
+            text-align: left;
+            border-bottom: 2px solid #ddd;
+        }
+
+        th {
+            background-color: #3498db;
+            color: white;
+        }
+
+        p {
+            margin: 5px 0;
+            color: #555;
+        }
+
+        a {
+            color: #3498db;
+            text-decoration: none;
+            font-weight: bold;
+            transition: color 0.3s;
+        }
+
+        a:hover {
+            color: #1d6fa5;
+        }
+
+        .info h1,
+        .info p {
+            margin-bottom: 10px;
+        }
+
+        .info p:last-child {
+            margin-bottom: 10px;
+        }
+
+    </style>
 </head>
 
 
 <body>
-    <div class="container">
-        <div class="user-info">
-            <br>
+<div class="container">
+        <div class="info">
             <h1>Data Pribadi</h1>
-            <!-- spasi -->
-        
             <?php $session = session() ?>
             <table>
-  
-    <tr>
-        <td><strong>Nama Lengkap  &nbsp;</strong></td>
-        <td><?= $session->user; ?></td>
-    </tr>
-    <tr>
-        <td><strong>Alamat</strong></td>
-        <td><?= $session->alamat; ?></td>
-    </tr>
-    <tr>
-        <td><strong>Email</strong></td>
-        <td><?= $session->email; ?></td>
-    </tr>
-    <tr>
-        <td><strong>Telepon</strong></td>
-        <td><?= $session->no_hp; ?></td>
-    </tr>
-</table>
-<!-- spasi -->
-                <p style="margin:5px; padding:15px;">&nbsp;&nbsp;&nbsp; 
+                <tr>
+                    <th>Nama Lengkap</th>
+                    <td><?= $session->user; ?></td>
+                </tr>
+                <tr>
+                    <th>Alamat</th>
+                    <td><?= $session->alamat; ?></td>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <td><?= $session->email; ?></td>
+                </tr>
+                <tr>
+                    <th>Telepon</th>
+                    <td><?= $session->no_hp; ?></td>
+                </tr>
+            </table>
             <h1>Keamanan</h1>
             <a href="/reset">Reset Password</a>
-            <br>
             <h1>Kamar Yang Disewa</h1>
         </div>
     </div>
